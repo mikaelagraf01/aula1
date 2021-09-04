363d4edf0ea40e5c858b3ddb1c80ebfe7291b785
Mikaela Kauana Griebler Graf <mg0125164@>
2021-09-04 09:22:31 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _18anos
+{
+    public class Cidade
+    {
+       public int Cep { get; set; }
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
